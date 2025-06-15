ee5f2cb68b0dc05356460030eb3adde2387f4b84
SHUBHAM KUMAR <shubhamkumar13504@gmail.com>
2025-06-16 01:30:43 +0530

    Commit Added

--- problem1.py
+++ problem1.py
@@ -1,17 +1,19 @@
-l1=[2,7,11,15]
-target=9
-def func(l1,target):
-    for i,num in enumerate(l1):
-        dict={}
-        complemet=num-target
-        if complemet in dict:
-            return(dict[complemet],i)
-        dict[num]=i
+# l1=[2,7,11,15]
+# target=9
+# THESE ARE MY MISTAKES THAT I HAVE DONE IN THIS
+# def func(l1,target):
+#     for i,num in enumerate(l1):
+#         dict={}
+#         complemet=num-target       #SUBSTARCT OPOSITE
+#         if complemet in dict:
+#             return(dict[complemet],i)    #RETURN TYPE TUPLE []<--()
+#         dict[num]=i
 
-def twoSum(self, l1, target):
-        seen = {}
-        for i, num in enumerate(l1):
-            complement = target - num
-            if complement in seen:
-                return [seen[complement], i]
-            seen[num] = i
+class Solution:    # we must have to wrap in a class 
+ def twoSum(self,nums,target):
+     dict={}
+     for i,num in enumerate(nums):
+        complemet=target-num
+        if complemet in dict:
+            return[dict[complemet],i]
+        dict[num]=i
